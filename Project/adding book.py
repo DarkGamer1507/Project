aefb8ef43d64ea9dbd51296c0e9bad402d5c20af
Rudransh <rudranshaggarwal1507@gmail.com>
2022-05-29 22:52:24 +0530

    added book's database and adding book function

---
+++ Project/adding book.py
@@ -0,0 +1,60 @@
+import csv
+
+def book_add():
+
+    # used for adding a new entry to the student's database
+
+    print("ADDING A NEW BOOK")
+    print("--------------------------------------------------------------------------------")
+
+    l1 = []
+    check = 0 # to confirm if student is added or not
+
+    while True:
+        l1.append(input("Serial Number: "))
+        l1.append(input("Author: "))
+        l1.append(input("Book name: "))
+        l1.append(input("Genre: "))
+        l1.append(input("Current Book Status (available/rented): "))
+        l1.append(input("Rented by (enter students addmission no. / none): "))
+
+        print("Addmission number:",l1[0] ,
+        "\nName:",l1[1] ,
+        "\nClass:",l1[2] ,
+        "\nGenre:",l1[3],
+        "\nBook Status:",l1[4],
+        "\nRented by:" ,l1[5],) 
+        # to make sure corrrect information has been inputed
+
+        x = input("Continue (n/y): ")
+
+        if x in ('y', 'Y'):
+            check += 1
+            break
+            
+        else:
+            con = input("\nRe-Enter data? (y/n)\n")
+            if con in ('y', 'Y'):
+                l1 = []
+                continue
+            else:
+                l1 = []
+                break           
+
+
+    with open("books.csv", 'a', newline='') as file_books:
+        student_writer = csv.writer(file_books)
+        student_writer.writerow(l1)
+    
+    if check != 0:
+        print("\nBook Added!\n")
+        
+    cho = input("Add another entry? (y/n) ")
+    if cho in ('y','Y'):
+        book_add()
+        
+    else:
+        return 
+    
+
+book_add()
