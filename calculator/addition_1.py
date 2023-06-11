4f0e28e850f613040f971bb8301c3db6ca386f8b
Rudransh Aggarwal <rudranshaggarwal1507@gmail.com>
2023-06-11 23:05:30 +0530

    calculator done

---
+++ calculator/addition_1.py
@@ -0,0 +1,8 @@
+def add():
+    x = int(input('Enter first number: '))
+    y = int(input('Enter second number: '))
+    z = x + y
+
+    print("Sum of ",x,'and ', y,'is =',z)
+
+add()
---
+++ calculator/subtraction_1.py
@@ -0,0 +1,8 @@
+def sub():
+    x = int(input('Enter first number: '))
+    y = int(input('Enter second number: '))
+    z = x - y
+
+    print("Difference of ",x,'and ', y,'is =',z)
+
+sub()
